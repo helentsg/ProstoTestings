f3dfbac94e24dcac542710966694c790db4ed4bc
Elena Tsegelnik <helentsg@gmail.com>
2021-01-23 16:46:51 +0500

    Initial Commit

---
+++ ProstoTestings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProstoTestings
+//
+//  Created by  Елена on 23.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
