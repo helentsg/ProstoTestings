30f18160a033f0d44bdf40da027d4bdcb537a168
Elena Tsegelnik <helentsg@gmail.com>
2021-01-25 15:31:19 +0500

    changes

--- ProstoTestings/Services/ImageLoader.swift
+++ ProstoTestings/Services/ImageLoader.swift
@@ -10,62 +10,82 @@ import UIKit
 class ImageLoader {
     
     static let shared = ImageLoader()
-    var imageCache = NSCache<NSURL, UIImage>()
+    var cachedImages = NSCache<NSURL, UIImage>()
+    private var loadingResponses = [NSURL: [(Result<(Item, UIImage?), NetworkRequestError>) -> Void]]()
+    
+    final func image(url: NSURL) -> UIImage? {
+        return cachedImages.object(forKey: url)
+    }
     
     private init() {
         
     }
     
+    // Returns the cached image if available, otherwise asynchronously loads and caches it.
     func downloadImage(withURL url: URL, forItem item: Item, completion: @escaping (Result<(Item, UIImage?), NetworkRequestError>) -> Void) {
         
-        if let imageFromCache = imageCache.object(forKey: url as NSURL) {
-            
-            completion(.success((item, imageFromCache)))
-            
-        } else {
-            
-            let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 10)
-            URLSession.shared.dataTask(with: request) {[weak self] (data, response, error) in
-                
-                guard let self = self else {
-                    return
-                }
-                
-                if let error = error {
-                    /// No Internet Connection
-                    if error._code == NSURLErrorNotConnectedToInternet {
-                        DispatchQueue.main.async {
-                            completion(.failure(.offline))
-                        }
-                        /// Другая ошибка
-                    } else {
-                        DispatchQueue.main.async {
-                            completion(.failure(.error(description: error.localizedDescription)))
-                        }
-                    }
-                    
-                }
-                guard let httpResponse = response as? HTTPURLResponse,
-                      (200...299).contains(httpResponse.statusCode) else {
-                    return
-                }
-                guard let data = data else {
-                    return
-                }
-                
-                guard let imageToCache = UIImage(data: data) else {
-                    return
-                }
-                
-                self.imageCache.setObject(imageToCache, forKey: url as NSURL)
-                
-                DispatchQueue.main.async {
-                    
-                    completion(.success((item, imageToCache)))
-                }
-                
-            }.resume()
+        let url = url as NSURL
+        
+        // Check for a cached image.
+        if let cachedImage = image(url: url) {
+            DispatchQueue.main.async {
+                completion(.success((item, cachedImage)))
+            }
+            return
         }
+        
+        // In case there are more than one requestor for the image, we append their completion block.
+        if loadingResponses[url] != nil {
+            loadingResponses[url]?.append(completion)
+            return
+        } else {
+            loadingResponses[url] = [completion]
+        }
+        
+        // Go fetch the image.
+        ImageURLProtocol.urlSession().dataTask(with: url as URL) {[weak self] (data, response, error) in
+            
+            guard let self = self else {
+                return
+            }
+            
+            if let error = error {
+                                /// No Internet Connection
+                                if error._code == NSURLErrorNotConnectedToInternet {
+                                    DispatchQueue.main.async {
+                                        completion(.failure(.offline))
+                                        return
+                                    }
+                                    /// Другая ошибка
+                                } else {
+                                    DispatchQueue.main.async {
+                                        completion(.failure(.error(description: error.localizedDescription)))
+                                        return
+                                    }
+                                }
+                            }
+
+            guard let responseData = data,
+                  let image = UIImage(data: responseData),
+                  let blocks = self.loadingResponses[url],
+                  error == nil else {
+                DispatchQueue.main.async {
+                    completion(.failure(.error(description: "Ошибка загрузки")))
+                }
+                return
+            }
+            
+            // Cache the image.
+            self.cachedImages.setObject(image, forKey: url)
+            
+            for block in blocks {
+                DispatchQueue.main.async {
+                    block(.success((item, image)))
+                }
+                return
+            }
+            
+        }.resume()
     }
     
 }
